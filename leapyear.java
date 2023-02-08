5e0b14a49ff200d3e3f3fa8933257577ad9136d2
Subhash <barelasubhsh16@gmail.com>
2023-02-08 14:55:06 +0530

    this is leap year program

---
+++ leapyear.java
@@ -0,0 +1,32 @@
+public class Main {
+  public static void main(String[] args) {
+  int year = 2016;
+  if((year%400 == 0){
+    System.out.println(year +" leap year");
+     }
+     else if(year%4 == 0)
+     {
+        if(year%100 != 0)
+           {
+             System.out.printl(year +" leap year");
+            }
+       else{
+            System.out.prinln(year +"not leap year");
+          }
+       }
+ else{
+       System.out.println(year +" not leap year");
+      }
+   }
+  }
+
+...................................
+int year = 2016;
+if((year%400 == 0)||(year%4 == 0 && year%100 != 0)
+{
+System.out.println("leap year");
+}
+else
+{
+System.out.println("not leap year");
+}
